c35e9c558132f95edf01447a2fc49396c5aafd90
EmblemPrograms <EmblemPrograms@users.noreply.github.com>
2026-08-23 20:33:49 +0100

    Add JavaScript demo pages and examples
    This change introduces basic JavaScript practice examples in js/app.js, including loops and conditionals, and updates the landing page to a simple browser-based date demo. It also revises the media page content to include a responsive CSS grid shop layout and adds a second copy of that markup appended to the file, creating a mixed lesson/demo page structure.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,78 @@
-const a = 10
-const b = 5
-const x = 6
-const c = 10 
+// For loop
+for (let i = 1; i <=10; i++) {
+    console.log(
+        "Numbers: " + i
+    )
+}
 
-console.log (a < b)
-console.log (a > b)
-console.log (a < x)
-console.log (a > x)
-console.log (a <= b)
-console.log (a >= b)
-console.log (a <= c)
-console.log (a >= x)
 
-const Name = `Olajide Fawwas`
-const age = 2026 -1999
-// const day = 'Friday'
-// const week = null
-// let studentCount
-console.log("My Name is " + Name)
-console.log("I am "  + age + " years old")
+// While Loop 
+// let count = 1
+
+// while (count <=10) {
+//     console.log("Count is: " + count) 
+//     count = count + 1
+// }
+
+
+// If and Else If Statement
+// let score  = 68
+
+// console.log("Your Score is " + score)
+// if (score >= 90) {
+//     console.log("Grade: A")
+// } else if (score >= 70) {
+//     console.log("Grade: B")
+// } else if (score >= 60) {
+//     console.log("Grade: C")
+// }
+// else {
+//     console.log("Grade: Needs Improvement")
+// }
+
+
+// const todayClass = 'Class 17'
+// let topic = "JavaScript Basics"
+// topic = "If & Loop Statement"
+// topic = "Switch Statement"
+// const name = 'Anthony'
+// const message = `Welcome, ${name}!`
+// let Age = 2026 - 1999
+// let Age2 = 2026 - 2010
+// const hasID = true
+
+// console.log ("Today's class is the " + todayClass)
+// console.log('The topic for today is ' + topic)
+// console.log(message)
+// console.log("I am " + Age + ' years old today')
+// console.log("I am " + Age2 + ' years old today')
+// console.log(Age >= 18 == hasID)
+// console.log(Age < 18 != hasID)
+// console.log(Age2 >= 18 == hasID)
+// console.log(Age2 <= 18 != hasID)
+
+// const a = 10
+
+// const b = 5
+// const x = 6
+// const c = 10 
+
+// console.log (a < b)
+// console.log (a > b)
+// console.log (a < x)
+// console.log (a > x)
+// console.log (a <= b)
+// console.log (a >= b)
+// console.log (a <= c)
+// console.log (a >= x)
+
+// const Name = `Olajide Fawwas`
+// const age = 2026 -1999
+// // const day = 'Friday'
+// // const week = null
+// // let studentCount
+// console.log("My Name is " + Name)
+// console.log("I am "  + age + " years old")
 // console.log(day.toUpperCase())
 // console.log(week)
 // console.log(studentCount + " " + "Students Attended today's Class")
@@ -31,8 +85,11 @@ console.log("I am "  + age + " years old")
 // let divide = a / b
 // let remiander = a % b
 // let exponential = a ** b
+// let areaofatriangle = 0.5 * a * b
 // let count = 5
 // count--
+// let count2 = 6
+// count2++
 // console.log (sum)
 // console.log (diff)
 // console.log (multiply)
@@ -40,3 +97,5 @@ console.log("I am "  + age + " years old")
 // console.log (remiander)
 // console.log (exponential)
 // console.log(count)
+// console.log(count2)
+// console.log(areaofatriangle)
